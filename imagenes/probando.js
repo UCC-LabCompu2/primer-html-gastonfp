a855138bd32095d63d40240c0fd337c3b2c67490
agustinmayorga76 <agumayorga@hotmail.com>
2017-05-04 09:45:37 -0300

    java

---
+++ imagenes/probando.js
@@ -0,0 +1,14 @@
+/*
+ * sumatoria de dos numeros
+ * @metbod calcular
+ * @param num1
+ * @param num2
+ * @return total
+ */
+
+
+function calcular(num1, num2) {
+    var total;
+    total = num1 + num2;
+    return total;
+}
